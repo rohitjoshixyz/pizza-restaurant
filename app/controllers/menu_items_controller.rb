7e8c66511951d73409c27a0773adf22d0ce92048
Rohit Joshi <rohit.joshi@joshsoftware.com>
2020-05-24 22:36:42 +0530

    Completed CRUD operations of Menu item - Added link to source code - Add html to display toppings - Modify CSS of menu_item form and list - Add default image for pizza if not uploaded - Add Veg or Non-veg in Menu

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,4 +1,6 @@
 class MenuItemsController < ApplicationController
+  before_action :load_menu_item, only: [:edit, :update, :show, :destroy]
+
   def index
     @menu_items = MenuItem.all
   end
@@ -14,9 +16,31 @@ class MenuItemsController < ApplicationController
     redirect_to menu_items_path
   end
 
+  def edit
+  end
+
+  def update
+    @menu_item.update(menu_item_params)
+    flash[:success] = "Menu Item updated successfully"
+    redirect_to menu_items_path
+  end
+
+  def show
+  end
+
+  def destroy
+    @menu_item.destroy
+    flash[:info] = "Menu Item deleted successfully"
+    redirect_to menu_items_path
+  end
+
   private
 
   def menu_item_params
     params.require(:menu_item).permit(:name, :description, :topping_ids, :size, :price, :category, :photo_url, :photo, toppings_attributes: [:id, :name, :veg, :_destroy])
+  end
+
+  def load_menu_item
+    @menu_item = MenuItem.find(params[:id])
   end
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,5 +1,11 @@
 class MenuItem < ApplicationRecord
+  after_save :attach_default_photo
+
   has_many :toppings, :dependent => :destroy
   has_one_attached :photo
   accepts_nested_attributes_for :toppings, allow_destroy: true
+
+  def attach_default_photo
+    self.photo.attach(io: File.open('app/assets/images/default_pizza_photo.png'), filename: 'default.png') unless self.photo.attached?
+  end
 end
