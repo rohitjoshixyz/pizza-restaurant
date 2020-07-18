4b4dc77975bf95e8116439e341ec2c159ca2dde2
Rohit Joshi <rohit.joshi@joshsoftware.com>
2020-07-18 21:39:26 +0530

    :bug: Fix default photo url bug

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -11,7 +11,7 @@ class MenuItem < ApplicationRecord
   def attach_default_photo
     unless photo.attached?
       photo.attach(io: File.open('app/assets/images/default_pizza_photo.png'), filename: 'default.png')
-      photo = url_for(photo)
+      # photo = url_for(photo)
     end
   end
 
